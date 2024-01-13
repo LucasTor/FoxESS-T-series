2bbe0b63d10cde6622d7544357142cd7420d2cec
Lucas Torresan <lucastorresan@hotmail.com>
2024-01-13 15:19:55 -0300

    fix: multiple socket connects

--- custom_components/foxess_tseries/sensor.py
+++ custom_components/foxess_tseries/sensor.py
@@ -74,7 +74,7 @@ async def async_setup_entry(
         nonlocal empty_attempts
 
         inverter_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        inverter_socket.settimeout(2)
+        inverter_socket.settimeout(5)
 
         try:
             _LOGGER.debug(f'Trying connection to FoxESS T Series on IP {host} and port {port}...')
